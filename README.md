2d03f50c59ab8a24b5ef4d4384c4fb6fc1c48f7a
anupa.kumar <anupa.kumar@cgi.com>
2021-04-10 01:13:07 +0200

    add crud oprations classes

---
+++ README.md
@@ -0,0 +1,5 @@
+# Spring Boot Practice Repo for Employee Manager Application
+---
+
+## What is does
+A repo to practice spring boot to create CRUD operations rest application to manage employees 
---
+++ src/main/java/com/anupam/employeemanager/model/Employee.java
@@ -0,0 +1,27 @@
+package com.anupam.employeemanager.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+@Data @AllArgsConstructor @NoArgsConstructor
+@Entity
+public class Employee implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+    private String name;
+    private String email;
+    private String jobTitle;
+    private String phone;
+    private String imageUrl;
+    @Column(nullable = false, updatable = false)
+    private String employeeCode;
+}
